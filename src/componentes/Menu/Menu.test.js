d9445b2a1c70d4d90270333533c517d85af0c04f
Luiz Claudio <lcluizclaudio96@gmail.com>
2023-09-18 18:51:16 -0300

    update

--- src/componentes/Menu/Menu.test.js
+++ src/componentes/Menu/Menu.test.js
@@ -1,24 +1,32 @@
 import { render } from '@testing-library/react';
 import Menu from './index';
+import { BrowserRouter } from 'react-router-dom';
+
+const renderComponent = (props) =>
+  render(
+    <BrowserRouter>
+      <Menu {...props} />
+    </BrowserRouter>,
+  );
 
 describe('Teste do componente Menu', () => {
   it('deve renderizar o link para pagina inicial', () => {
-    const { getByText } = render(<Menu />);
-    const linkElement = getByText(/inicial/i);
+    const { getByText } = renderComponent();
+    const linkElement = getByText(/início/i);
     expect(linkElement).toBeInTheDocument();
   });
   it('deve renderizar uma lista de links', () => {
-    const { getAllByRole } = render(<Menu />);
+    const { getAllByRole } = renderComponent();
     const links = getAllByRole('link');
     expect(links).toHaveLength(4);
   });
   it('Não deve renderizar o link para extrato', () => {
-    const { queryByText } = render(<Menu />);
+    const { queryByText } = renderComponent();
     const linkElement = queryByText(/extrato/i);
     expect(linkElement).not.toBeInTheDocument();
   });
   it('deve renderizar uma lista de links com a classe link', () => {
-    const { getAllByRole } = render(<Menu />);
+    const { getAllByRole } = renderComponent();
     const links = getAllByRole('link');
     links.forEach((link) => {
       expect(link).toHaveClass('link');
