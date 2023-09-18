2cad8e328b575a44b28bcdda2133176de44e9487
Luiz Claudio <lcluizclaudio96@gmail.com>
2023-09-18 15:57:53 -0300

    test: Saldo

---
+++ src/componentes/Principal/Saldo/Saldo.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import Saldo from './index';
+
+describe('Saldo', () => {
+    it('Deve renderizar o saldo com o valor monetario', () =>{
+        render(<Saldo saldo={1000} />);
+        const saldo = screen.getByTestId('saldo-conta');
+        expect(saldo).toBeInTheDocument();
+        expect(saldo.textContent).toBe('R$ 1000');
+        
+    })
+    
+});
