96cb55c1f22a9256289ffa38cd15aedc54c2ea74
Luiz Claudio <lcluizclaudio96@gmail.com>
2023-09-18 15:58:39 -0300

    test: cabeçalho

---
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Cabecalho from './index';
+
+test('deve renderizar o nome do usuario logado', () =>{
+    render(<Cabecalho />)
+    const nomeUsuario = screen.getByText(/Joana Fonseca Gomes/i);
+    expect(nomeUsuario).toBeInTheDocument();
+})
